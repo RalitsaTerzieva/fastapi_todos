84416f09581a196c49c7b31969bc81056a06fef2
Ralica Popova <ralicaterzieva90@gmail.com>
2026-04-30 15:51:59 +0300

    Add get todo by id

--- main.py
+++ main.py
@@ -21,3 +21,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get('/')
 def read_all(db: db_dependency):
     return db.query(models.Todos).all()
+
+@app.get('/todo/{todo_id}')
+def read_todo(todo_id:int, db: db_dependency):
+    return db.query(models.Todos).filter(models.Todos.id == todo_id).first()
