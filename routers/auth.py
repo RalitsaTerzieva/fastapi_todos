6d3f94cd18ddd9707fbd36c2d1f155b907aa0f28
Ralica Popova <ralicaterzieva90@gmail.com>
2026-05-26 17:32:46 +0300

    Update auth

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from models import Users
 
 router = APIRouter()
 
@@ -12,5 +13,15 @@ class CreateUserRequest(BaseModel):
     role: str
 
 @router.post('/auth')
-async def create_user():
-    return {"user": "authenticated"}
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True
+    )
+
+    return create_user_model
