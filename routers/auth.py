8e1f28a88e30064d4b0cfcd7a3441757334bd656
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-08 16:01:42 +0300

    Authenticate user

--- routers/auth.py
+++ routers/auth.py
@@ -33,7 +33,7 @@ def authenticate_user(username: str, password: str, db: db_dependency):
     user = db.query(Users).filter(Users.username == username).first()
     if not user: 
         return False
-    if not bcrypt_context.verify(password, user.hasshed_password):
+    if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return True 
 
@@ -55,4 +55,7 @@ async def create_user(db: db_dependency,create_user_request: CreateUserRequest):
 
 @router.post("/token")
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    return form_data.username
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user: 
+        return "Failed authentication!"
+    return "Successfull authentication!"
