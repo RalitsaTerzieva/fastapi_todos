ec79b29bc55a468d3c5311321a516d7c9103922f
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-09 16:58:14 +0300

    Add authorization to get todo by id

--- routers/todos.py
+++ routers/todos.py
@@ -30,6 +30,17 @@ class TodoRequest(BaseModel):
 def read_all(user: user_dependency, db: db_dependency):
     return db.query(models.Todos).filter(models.Todos.owner_id == user.get("id")).all()
 
+@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == user.get('id')).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
+
 @router.post('/todo', status_code=status.HTTP_200_OK)
 def create_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest):
     if user is None:
