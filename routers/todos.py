5c94232f95868a75d1ef8154232a63ebc28f5836
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-09 16:06:53 +0300

    Add user dependency in todo

--- routers/todos.py
+++ routers/todos.py
@@ -4,6 +4,7 @@ import models
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from .auth import get_current_user
 
 
 router = APIRouter()
@@ -17,6 +18,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
