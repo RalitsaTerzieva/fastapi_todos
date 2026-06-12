cb927803cee32f8b6e51b588af91bf0b0b457b1a
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-12 16:55:12 +0300

    Add phone number again

---
+++ alembic/versions/0d3ecef9d7bb_add_phone_number.py
@@ -0,0 +1,24 @@
+"""Add phone number
+
+Revision ID: 0d3ecef9d7bb
+Revises: 11f4d9d07e84
+Create Date: 2026-06-12 16:52:25.124103
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0d3ecef9d7bb'
+down_revision = '11f4d9d07e84'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
