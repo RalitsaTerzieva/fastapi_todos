a6e32e06938c52a0c3426a582aa9963ee91e0d70
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-13 18:26:37 +0300

    Add more unit tests

--- test/test_todos.py
+++ test/test_todos.py
@@ -33,8 +33,61 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 client = TestClient(app)
 
+@pytest.fixture
+def test_user():
+    user = Users(
+        id=1,
+        username="codingwithrobytest",
+        email="codingwithrobytest@email.com",
+        first_name="Eric",
+        last_name="Roby",
+        hashed_password=bcrypt_context.hash("testpassword"),
+        role="admin",
+        phone_number="(111)-111-1111"
+    )
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM users;"))
+        connection.commit()
 
-def test_read_all_authenticated():
+@pytest.fixture
+def test_todo(test_user):
+    todo = Todos(
+        title="Learn to code!",
+        description="Need to learn everyday!",
+        priority=5,
+        complete=False,
+         owner_id=test_user.id,
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+
+def test_read_all_authenticated(test_todo):
     response = client.get("/")
+
     assert response.status_code == status.HTTP_200_OK
-  
+
+    data = response.json()
+
+    assert len(data) == 1
+    assert data[0]["title"] == "Learn to code!"
+    assert data[0]["owner_id"] == test_todo.owner_id
+
+def test_read_one_authenticated(test_todo):
+    response = client.get(f"/todo/{test_todo.id}")
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    
+    assert data["title"] == "Learn to code!"
+    assert data["owner_id"] == test_todo.owner_id
