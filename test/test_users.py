2303714fcb42f8a7eb2b5ec9e91330fd2e68fb9a
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-14 18:50:49 +0300

    Add tests for get all users

---
+++ test/test_users.py
@@ -0,0 +1,19 @@
+from test.conftest import *
+from routers.users import get_db, get_current_user
+from fastapi import status
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+def test_return_user(test_user):
+    response = client.get("/users")
+  
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data['username'] == 'codingwithrobytest'
+    assert data['email'] == 'codingwithrobytest@email.com'
+    assert data['first_name'] == 'Eric'
+    assert data['last_name'] == 'Roby'
+    assert data['role'] == 'admin'
+    assert data['phone_number'] == '111-111-1111'
