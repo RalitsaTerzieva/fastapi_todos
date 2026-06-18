68572cbdfff79c05d78aee4b0c9a2ef101a53c21
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-18 12:21:24 +0300

    Add test for get user valid token

--- test/test_auth.py
+++ test/test_auth.py
@@ -22,3 +22,26 @@ def test_authenticate_user_success(test_user):
         assert authenticated_user.username == test_user.username
     finally:
         db.close()
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+
+    token = create_access_token(username, user_id, role, expires_delta)
+
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM],
+                               options={'verify_signature': False})
+
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    user = await get_current_user(token=token)
+    assert user == {'username': 'testuser', 'id': 1, 'user_role': 'admin'}
