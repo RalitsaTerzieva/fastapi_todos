9d90c49389313ad94dc256a89c6513e0d6eea9ab
Ralica Popova <ralicaterzieva90@gmail.com>
2026-06-18 14:11:20 +0300

    Start adding frontend logic

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 import models
 from database import engine
 from routers import admin, auth, todos, users
@@ -7,6 +10,18 @@ from routers import admin, auth, todos, users
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/home")
+def home(request: Request):
+    return templates.TemplateResponse(request=request, name="home.html")
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/home")
 
 @app.get("/healthy")
 def health_check():
